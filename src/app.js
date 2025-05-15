d6e63be9b206cc0d70469dfafc44068a9a82fb8b
AnitLearner <anitkumarbanka@gmail.com>
2025-05-15 12:31:12 +0530

    Learnt different api calls

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@
 
  const app = express();
 // Request Handler Listening to the request
+
+app.get("/home",(req,res) => {
+    console.log('Request received');
+    res.send('Hello!');
+});
+
+app.post("/home",(req,res) => {
+    console.log('Data received');
+    res.send('Hello Duniya!');
+});
+
 app.use("/test",(req,res) => {
     console.log('Request received');
     res.send('Hello, World Kya baat hai Helloo!');
