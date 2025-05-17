fc56d54e913cbf3cbe39363e4d00eb5c9f9bf5b9
AnitLearner <anitkumarbanka@gmail.com>
2025-05-17 19:54:51 +0530

    Login and Signup successfull

--- src/app.js
+++ src/app.js
@@ -3,15 +3,42 @@
  const app = express();
  const connectdb = require('./config/database');
  const User = require('./models/user');
+ const {validateSignUpData} = require('./utils/validation');
+ const bcrypt = require('bcrypt');
  app.use(express.json());
- app.post('/signup',async (req,res) => {
-  console.log(req.body);
-    
-    const user = new User(req.body);
+ // Need to creat the login api
+  app.post('/login', async (req, res) => {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      return res.status(401).send("Invalid email or password");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send("Invalid password");
+    }
+    res.send("Login successful");
+  });
+ app.post('/signup', async(req,res) => {
+  validateSignUpData(req);
+  const {firstName, lastName, emailId} = req.body;
+  const myPlaintextPassword = req.body.password;
+  const passwordHash = await bcrypt.hash(myPlaintextPassword, 10 );
+  const user = new User({
+    firstName,
+    lastName,
+    emailId,
+    password: passwordHash,
+  });
+  try{
     await user.save();
     res.send("User created successfully");
- })
-
+  }
+  catch(err){
+    res.status(400).send("Error saving the user");
+ }
+}
+)
  // Suppose we need to find from the database
  app.get('/user',async (req,res) => {
   const email = req.body.emailId;
