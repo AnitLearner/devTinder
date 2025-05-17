7caa86c9705c830271fc6e32300e93c7481a8090
AnitLearner <anitkumarbanka@gmail.com>
2025-05-17 08:32:31 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -10,6 +10,15 @@ app.get
     res.send('Hello, Admin!');
 });
 
+app.get('/user',(req,res) => {
+  try{
+    throw new Error("User not found");
+  }
+  catch(err){
+    res.status(500).send("Internal Server Error");
+  }
+})
+
 
  app.listen(3000, () => {
    console.log('Server is running on port 3000');
