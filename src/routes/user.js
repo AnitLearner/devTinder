c8e68c16f7fe73d5e78ca74a6ff22e7b558173aa
AnitLearner <anitkumarbanka@gmail.com>
2025-05-19 23:25:17 +0530

    Implemented Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -57,6 +57,10 @@ userRouter.get('/user/feed',userAuth, async(req,res) => {
         // 3. the users who have already sent him connection requests
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1)* limit;
         const connectionRequests = await ConnectionRequest.find({
             $or : [
                 {
@@ -81,7 +85,7 @@ userRouter.get('/user/feed',userAuth, async(req,res) => {
                 $ne : loggedInUser._id
             }}
         ]
-        }).select('firstName lastName');
+        }).select('firstName lastName').skip(skip).limit(limit);
         res.send(users);
     }
     catch(err){
